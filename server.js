ceb824cc784b380f7982c6dc64b727bc98b00f1c
ngoquocbao1010 <totti.chelsea@gmail.com>
2022-05-18 14:07:18 +0700

    🆕 feat: mock server for cardano service

--- server.js
+++ server.js
@@ -1,42 +1,50 @@
+import { createRequire } from "module";
 import express from "express";
 import bodyParser from "body-parser";
 import swaggerJsdoc from "swagger-jsdoc";
 import swaggerUiExpress from "swagger-ui-express";
+import cors from "cors";
 
 import didRouter from "./routes/did.js";
+import cardanoRouter from "./routes/cardano.js";
+
+const require = createRequire(import.meta.url);
+const cardanoServices = require("./swagger/cardano.json");
 
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors({ origin: "*" }));
 
 // Route
 app.use(didRouter);
+app.use(cardanoRouter);
 
 // Swagger config
-const swaggerDefinition = {
-    openapi: "3.0.0",
-    info: {
-        title: "Github Proxy API",
-        version: "1.0.0",
-    },
-    servers: [
-        {
-            url: "http://localhost:8080",
-            description: "Development server",
-        },
-    ],
-};
-
 const options = {
-    swaggerDefinition,
+    swaggerDefinition: {
+        openapi: "3.0.0",
+        info: {
+            title: "Github Proxy API",
+            version: "1.0.0",
+        },
+        servers: [
+            {
+                url: "http://localhost:8080",
+                description: "Github Database server",
+            },
+        ],
+    },
     apis: ["./routes/*.js"],
 };
 
-const swaggerSpec = swaggerJsdoc(options);
-app.use(
-    "/api-docs",
-    swaggerUiExpress.serve,
-    swaggerUiExpress.setup(swaggerSpec)
+const githubProxySwaggerSpec = swaggerJsdoc(options);
+app.use("/api-docs", swaggerUiExpress.serve, (...args) =>
+    swaggerUiExpress.setup(githubProxySwaggerSpec)(...args)
+);
+
+app.use("/api-cardano", swaggerUiExpress.serve, (...args) =>
+    swaggerUiExpress.setup(cardanoServices)(...args)
 );
 
 const port = 8080;
