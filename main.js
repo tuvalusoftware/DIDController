a8d67b5723acef413c0c6efe4c08c0f0e68301c2
ngoquocbao1010 <totti.chelsea@gmail.com>
2022-05-19 17:26:12 +0700

    🆕 feat: CRUD release

--- main.js
+++ main.js
@@ -2,13 +2,10 @@ import GithubDB from "./github_db/index.js";
 
 const main = async () => {
     try {
-        // const sha = await GithubDB.getLastCommitSHA("DID_Kukulu");
-        // console.log(sha);
+        const sha = await GithubDB.getLastCommitSHA("DID_Kukulu");
+        console.log(sha);
 
-        // const data = await GithubDB.tagACommit("v0.0.0.7");
-        // console.log(data);
-
-        const data = await GithubDB.getAllTags();
+        const data = await GithubDB.deleteARelease("test_release.v01");
         console.log(data);
     } catch (err) {
         console.log("ERROR from MAIN.JS: ", err);
