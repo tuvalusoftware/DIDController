e4bd7eb625e0b3f9320f4096396164505f5fdcea
ngoquocbao1010 <totti.chelsea@gmail.com>
2022-06-06 14:57:25 +0700

    👨‍💻 update: remove mock cardano service

--- server.js
+++ server.js
@@ -11,7 +11,6 @@ import logger from "./logger.js";
 
 const require = createRequire(import.meta.url);
 const services = require("./swagger/did_controller.json");
-const cardanoServices = require("./swagger/cardano.json");
 
 const app = express();
 app.use(bodyParser.json());
@@ -44,10 +43,6 @@ app.use((err, req, res, next) => {
 
 app.use("/api-docs", swaggerUiExpress.serve, (...args) =>
     swaggerUiExpress.setup(services)(...args)
-);
-
-app.use("/api-cardano", swaggerUiExpress.serve, (...args) =>
-    swaggerUiExpress.setup(cardanoServices)(...args)
 );
 
 const port = 9000;
