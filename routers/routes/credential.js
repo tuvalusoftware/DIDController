d08d25163dab6ec98971cfb9fd002ce44620d701
ngoquocbao1010 <totti.chelsea@gmail.com>
2022-12-01 16:56:02 +0700

    🆕 feat: get all credentials API

--- routers/routes/credential.js
+++ routers/routes/credential.js
@@ -1,11 +1,12 @@
 import express from "express";
-import credentialController from "../controllers/credential.js";
 import authController from "../controllers/auth.js";
+import credentialController from "../controllers/credential.js";
 
 const router = express.Router();
 
 router.use(authController.ensureSecurityServiceAuthentication);
 
+router.route("/all").get(credentialController.getAll);
 router
     .route("/")
     .get(credentialController.getCredentialByHash)
