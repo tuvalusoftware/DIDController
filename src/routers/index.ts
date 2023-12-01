3fcc4aa8cc4ef8ae23ce8758fbb185f1d5c17129
HaoHuynh0301 <hao152903@gmail.com>
2023-12-01 11:12:24 +0700

    refactor: add v2.

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -5,7 +5,7 @@ import didRoute from "./routes/did.route";
 import didDocRoute from "./routes/doc.route";
 
 export default (app: Express) => {
-  app.use("/api/did", didRoute);
-  app.use("/api/credential", creRoute);
-  app.use("/api/doc", didDocRoute);
+  app.use("/api/v2/did", didRoute);
+  app.use("/api/v2/credential", creRoute);
+  app.use("/api/v2/doc", didDocRoute);
 };
